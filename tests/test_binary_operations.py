ab26b5e3ae8f293712fccf245be1bb4fb4bb10ee
CodexSandboxOffline <rainbowmyreserved@gmail.com>
2026-08-26 14:02:30 +0400

    test: isolate bounded type xref coverage

--- tests/test_binary_operations.py
+++ tests/test_binary_operations.py
@@ -46,54 +46,6 @@ def _load_binary_operations_module():
     return module, info_logs
 
 
-class FakeView:
-    def __init__(self, filename):
-        self.file = types.SimpleNamespace(filename=filename)
-
-
-class ConstantTimeSizeView:
-    def __init__(self, width=None):
-        self.width = width
-
-    def get_data_var_at(self, _address):
-        if self.width is None:
-            return None
-        return types.SimpleNamespace(type=types.SimpleNamespace(width=self.width))
-
-    def get_type_at(self, _address):
-        return None
-
-    @property
-    def functions(self):
-        raise AssertionError("size inference must not scan functions")
-
-
-class PaginatedDataView:
-    def __init__(self):
-        self.iterated = 0
-
-    @property
-    def data_vars(self):
-        def addresses():
-            for address in range(0x1000, 0x1100):
-                self.iterated += 1
-                yield address
-
-        return addresses()
-
-    def get_data_var_at(self, _address):
-        return types.SimpleNamespace(type=types.SimpleNamespace(width=1))
-
-    def read_int(self, address, _width):
-        return address & 0xFF
-
-    def read(self, address, length):
-        return bytes([address & 0xFF]) * length
-
-    def get_symbol_at(self, address):
-        return types.SimpleNamespace(name=f"data_{address:x}", raw_name=f"data_{address:x}")
-
-
 class IndexedFieldXrefView:
     def __init__(self):
         self.code_query = None
@@ -200,43 +152,6 @@ class BinaryOperationsTests(unittest.TestCase):
     def setUp(self):
         self.info_logs.clear()
         self.operations = self.module.BinaryOperations(object())
-
-    def test_first_assignment_remains_active_and_repeat_is_noop(self):
-        view = FakeView("sample.bndb")
-
-        self.operations.current_view = view
-        self.operations.current_view = view
-
-        self.assertIs(self.operations.current_view, view)
-        self.assertEqual(len(self.operations._views_by_id), 1)
-        self.assertEqual(self.info_logs, ["Set current binary view: sample.bndb"])
-
-    def test_replacement_view_for_same_file_updates_silently(self):
-        first_view = FakeView("sample.bndb")
-        replacement_view = FakeView("sample.bndb")
-
-        self.operations.current_view = first_view
-        self.operations.current_view = replacement_view
-
-        self.assertIs(self.operations.current_view, replacement_view)
-        self.assertEqual(len(self.operations._views_by_id), 1)
-        self.assertEqual(self.info_logs, ["Set current binary view: sample.bndb"])
-
-    def test_size_inference_never_scans_program_functions(self):
-        self.operations._current_view = ConstantTimeSizeView(width=8)
-        self.assertEqual(self.operations.infer_data_size(0x1000), 8)
-
-        self.operations._current_view = ConstantTimeSizeView()
-        self.assertIsNone(self.operations.infer_data_size(0x1000))
-
-    def test_defined_data_applies_pagination_during_iteration(self):
-        view = PaginatedDataView()
-        self.operations._current_view = view
-
-        items = self.operations.get_defined_data(offset=1, limit=2, read_len=1)
-
-        self.assertEqual([item["address"] for item in items], ["0x1001", "0x1002"])
-        self.assertLessEqual(view.iterated, 4)
 
     def test_field_xrefs_use_bounded_binary_ninja_indexes(self):
         view = IndexedFieldXrefView()
